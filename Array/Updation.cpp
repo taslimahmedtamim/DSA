3664b52a16c947e295faec5cacdae98dbcad15bb
taslimahmedtamim <taslimahmedtamim4u@gmail.com>
2025-06-04 01:27:28 +0600

    Add Array Updation code

---
+++ Array/Updation.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i]; 
+    }
+
+    int position, value;
+
+    cout<<"Enter the position (Index no.) : ";
+    cin>>position;
+    cout<<"Enter the updated Value : " ;
+    cin>>value;
+
+    arr[position] = value;
+
+    for(int i = 0; i<n; i++){
+        cout<<arr[i];
+        if(i<n-1){
+            cout<<",";
+        }
+    }
+    return 0;
+}
