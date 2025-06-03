36e37d04115eb29c9dc7f5a981ca6df4c2c40e35
taslimahmedtamim <taslimahmedtamim4u@gmail.com>
2025-06-04 01:58:14 +0600

    add size,deletion,insertion code

---
+++ Array/Deletion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i = 0; i<n; i++){
+        cin>>arr[i]; 
+    }
+    
+    int position;
+
+    cout<<"Enter the position to delete an element : ";
+    cin>>position;
+    
+    for(int i = position; i<n; i++){
+        arr[i] = arr[i+1]; 
+    }
+
+
+    for(int i = 0; i<n-1; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ Array/Size.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,5,8,9};
+    
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<size<<endl;
+    return 0;
+}
