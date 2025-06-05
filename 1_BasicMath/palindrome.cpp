c9e07641df29c49abc5162e6ded168bd9daca8a6
taslimahmedtamim <taslimahmedtamim4u@gmail.com>
2025-06-05 22:10:37 +0600

    palindrome code

---
+++ 1_BasicMath/palindrome.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverseNum(int n){
+    int s = 0;
+    while(n>0){
+        int rem = n%10;
+        s = (s*10) + rem;
+        n=n/10;
+    }
+    return s;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int revN = reverseNum(n);
+    if(n==revN){
+        cout<<"palindrome"<<"\n";
+    }
+    else{
+        cout<<"Not palindrome"<<"\n";
+    }
+    return 0;
+}
