88f747763cc58d63f82cb718e9e88ed7fd606732
taslimahmedtamim <taslimahmedtamim4u@gmail.com>
2025-06-13 02:59:30 +0600

    string

---
+++ 3_String/StringReverse.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+    string str;
+    string reverse_str;
+    getline(cin , str);
+    for(int i = str.size()-1; i>=0; i--){
+        reverse_str.push_back(str[i]);
+    }
+    cout<<reverse_str;
+
+    return 0;
+}
---
+++ 3_String/StringSize.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+
+    string s = "Taslim Ahmed Tamim";
+    int len = s.size();
+    cout<<len<<"\n";
+    return 0;
+}
