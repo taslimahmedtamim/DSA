7eccd3fdbdc9f8d3c5dde04df05d8d692739fcaa
Taslim Ahmed Tamim <taslimahmedtamim4u@gmail.com>
2025-10-22 20:09:05 +0600

    Refactor cp.cpp to add snippet template
    Transformed the main function and included a snippet template for C++ boilerplate code.

--- 0_CP_template/cp.cpp
+++ 0_CP_template/cp.cpp
@@ -1,16 +1,46 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define endl "\n"
-#define ll long long
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+// #define endl "\n"
+// #define ll long long
 
-int main(){
-    fastio;
+// int main(){
+//     fastio;
 
-    int t;
-    cin>>t;
-    while(t--){
+//     int t;
+//     cin>>t;
+//     while(t--){
         
-    }
-    return 0;
+//     }
+//     return 0;
+// }
+
+
+{
+	// Place your snippets for cpp here. Each snippet is defined under a snippet name and has a prefix, body and
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the
+	// same ids are connected.
+	// Example:
+	"cpp": {
+		"prefix": "cpp_boilerplate",
+		"body": [
+			"#include<bits/stdc++.h>",
+			"using namespace std;",
+			"#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);",
+			"#define endl \"\\n\" ",
+			"",
+			"int main(){",
+			"   fastio;",
+			"",
+			"   int t;",
+			"   cin>>t;",
+			"   while(t--){",
+			"       $1",
+			"   }",
+			"   return 0;",
+			"}"
+		],
+		"description": "Boilerplate for Ccompetitive programmers in c++"
+	}
 }
