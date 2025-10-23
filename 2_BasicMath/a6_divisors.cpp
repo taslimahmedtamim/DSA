409fc423ce15428278c7cbc88219072717472f9d
Taslim Ahmed Tamim <taslimahmedtamim4u@gmail.com>
2025-10-23 12:13:15 +0600

    Implement optimized divisors function
    Added optimized function to calculate divisors.

--- 2_BasicMath/a6_divisors.cpp
+++ 2_BasicMath/a6_divisors.cpp
@@ -1,16 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
 
+void bruteForceDivisors(){
     int n;
     cin>>n;
+    int count = 0;
     for(int i = 1; i<=n; i++){
         if(n%i==0){
             cout<<i<<" ";
+            count++;
         }
     }
+    cout<<"Total divisors: "<<count<<endl;
+}
+// Time complexity : O(n)
+
+
+void optimizedDivisors(){
+    int n;
+    cin>>n;
+    int count = 0;  
+    for(int i = 1; i*i<=n; i++){
+        if(n%i==0){
+            cout<<i<<" ";
+            count++;
+            if(n/i!=i){
+                cout<<n/i<<" ";
+                count++;
+            }
+        }
+    }
+    
+    cout<<"Total divisors: "<<count<<endl;
+}
+// Time complexity : O(sqrt(n))
+
+
+int main(){
+
+    // bruteForceDivisors();
+    optimizedDivisors();
     return 0;
 }
-
-// Time complexity : O(n)
